31b79e2d49146fe0a85b33412d86c8cf745b8db0
Alda Vigdis Skarphedinsdottir <aldavigdis@aldavigdis.is>
2025-09-30 19:03:12 +0200

    Bumping version to 0.5.0-beta1 (#22)
    * Bumping version to 0.5.0-beta1
    
    * Bumping required PHP version
    
    * Fixing a little bug in the Docker setup

--- connector-for-dk.php
+++ connector-for-dk.php
@@ -4,9 +4,9 @@
  * Plugin Name: Connector for DK
  * Plugin URI: https://github.com/aldavigdis/connector-for-dk/
  * Description: Sync your WooCommerce store with DK, including prices, inventory status and generate invoices for customers on checkout.
- * Version: 0.4.6
+ * Version: 0.5.0-beta1
  * Requires at least: 6.1.5
- * Requires PHP: 8.0
+ * Requires PHP: 8.2
  * Author: Alda Vigdis
  * Author URI: https://aldavigdis.is
  * License: GPL-3.0-or-later

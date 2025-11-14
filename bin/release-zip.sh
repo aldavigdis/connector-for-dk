7aaf56015aa2f989791308d49e1e765312e58ec7
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2025-11-14 10:36:43 +0100

    Finishing cleanup of build script

--- bin/release-zip.sh
+++ bin/release-zip.sh
@@ -7,6 +7,17 @@ rm -rf vendor/
 composer install --no-dev
 
 cd .. && zip -r connector-for-dk.zip connector-for-dk \
-             -x connector-for-dk/.git/\* connector-for-dk/.* connector-for-dk/.*\* connector-for-dk/dockpress-secrets/\*
+             -x connector-for-dk/.git/\* \
+			 connector-for-dk/tests/*\* \
+			 connector-for-dk/*.xml \
+			 connector-for-dk/.* \
+			 connector-for-dk/.*\* \
+			 connector-for-dk/dockpress-secrets/\* \
+			 connector-for-dk/dockpress-secrets/ \
+			 connector-for-dk/bin/\* \
+			 connector-for-dk/bin/ \
+			 connector-for-dk/languages/*.*~ \
+			 connector-for-dk/assets/screenshot-*.png \
+			 connector-for-dk/static/
 
 mv connector-for-dk.zip "./connector-for-dk/connector-for-dk-pro-v$version.zip"

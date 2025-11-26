d479ae5a5d0e5515f2147497224e08867c923dd0
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2025-11-26 22:42:07 +0100

    Fixing the cron job for handling expired licenses

--- src/Cron/Hourly.php
+++ src/Cron/Hourly.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace AldaVigdis\ConnectorForDK\Cron;
 
 use AldaVigdis\ConnectorForDK\Config;
+use AldaVigdis\ConnectorForDK\License;
 use AldaVigdis\ConnectorForDK\Import\SalesPayments as ImportSalesPayments;
 use AldaVigdis\ConnectorForDK\Import\Currencies as ImportCurrencies;
 use AldaVigdis\ConnectorForDK\Import\Customers as ImportCustomers;
@@ -23,6 +24,10 @@ class Hourly {
 	 * Saves all products from the DK API.
 	 */
 	public static function run(): void {
+		if ( ! License::is_ok() ) {
+			return;
+		}
+
 		if ( Config::get_dk_api_key() && Config::get_enable_cronjob() ) {
 			ImportSalesPayments::get_methods();
 			ImportCurrencies::save_all_from_dk();

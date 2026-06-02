e0b2d5b3bb36ea59730a89ad82ef0bb16e2d8748
Alda Vigdis <aldavigdis@aldavigdis.is>
2026-06-03 00:01:04 +0200

    Improving invoices cron job
    - Improving testability of the cron job
    - Adding license and config conditions

--- src/Cron/PostInvoices.php
+++ src/Cron/PostInvoices.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace AldaVigdis\ConnectorForDK\Cron;
 
+use AldaVigdis\ConnectorForDK\Config;
+use AldaVigdis\ConnectorForDK\License;
 use AldaVigdis\ConnectorForDK\OrderStatus;
 use AldaVigdis\ConnectorForDK\Helpers\Order as OrderHelper;
 use WC_Order;
@@ -23,18 +25,15 @@ class PostInvoices implements CronJobTemplate {
 	 * Run hourly task
 	 */
 	public static function run(): void {
-		$the_past      = gmdate( 'r', time() - self::PAST_ORDER_LIMIT );
-		$recent_orders = wc_get_orders(
-			array(
-				'status'       => array( 'completed', 'processing' ),
-				'meta_key'     => 'connector_for_dk_invoice_number',
-				'meta_compare' => 'NOT EXISTS',
-				'date_query'   => array( 'after' => $the_past ),
-				'order'        => 'ASC',
-				'orderby'      => 'ID',
-				'limit'        => self::GET_ORDERS_LIMIT,
-			)
-		);
+		if ( ! License::is_ok() ) {
+			return;
+		}
+
+		if ( ! ( Config::get_dk_api_key() && Config::get_enable_cronjob() ) ) {
+			return;
+		}
+
+		$recent_orders = self::recent_orders();
 
 		foreach ( $recent_orders as $order ) {
 			if ( ! $order instanceof WC_Order ) {
@@ -57,5 +56,28 @@ class PostInvoices implements CronJobTemplate {
 
 			OrderStatus::maybe_send_invoice( $order, false );
 		}
+	}
+
+	/**
+	 * Get orders to run through
+	 *
+	 * Determines the recent orders to check if invoice generation attempts have
+	 * been exhausted. This maxes out at 50 orders within the past 48 hours.
+	 */
+	public static function recent_orders(): array {
+		$the_past      = gmdate( 'r', time() - self::PAST_ORDER_LIMIT );
+		$recent_orders = wc_get_orders(
+			array(
+				'status'       => array( 'completed', 'processing' ),
+				'meta_key'     => 'connector_for_dk_invoice_number',
+				'meta_compare' => 'NOT EXISTS',
+				'date_query'   => array( 'after' => $the_past ),
+				'order'        => 'ASC',
+				'orderby'      => 'ID',
+				'limit'        => self::GET_ORDERS_LIMIT,
+			)
+		);
+
+		return $recent_orders;
 	}
 }
